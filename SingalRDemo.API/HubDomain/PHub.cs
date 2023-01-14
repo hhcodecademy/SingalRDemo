d17b7d693c4a647495df70f635d8a0bccd9a823b
hhcodecademy <huseyn.hasanli@code.edu.az>
2023-01-14 20:03:14 +0400

    Add project files.

---
+++ SingalRDemo.API/HubDomain/PHub.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SingalRDemo.API.HubDomain
+{
+    public class PHub : Hub
+    {
+        public async Task sendMessage(string message)
+        {
+          
+
+            await Clients.All.SendAsync("RecieveMessage", message);
+
+        }
+    }
+}
